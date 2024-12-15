836f68da86f0e9327ee6ff4936d81e97c943f0e3
Kaizar Kanchwala <kk840@cam.ac.uk>
2024-12-15 23:44:12 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "React", "NodeJS"],
+        address: {
+            city: "New York",
+            country: "USA"
+        }
+    }
+    return member;
+}
